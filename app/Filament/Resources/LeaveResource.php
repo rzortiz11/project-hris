b5416d24687c978b8e89e64d583b6cdec5f99612
Raymart Ortiz <rzortiz@morepower.ph>
2024-04-07 22:13:34 +0800

    comment the filament-resource for Absence and Leave Resource

--- app/Filament/Resources/LeaveResource.php
+++ app/Filament/Resources/LeaveResource.php
@@ -1,68 +1,68 @@
 <?php
 
-namespace App\Filament\Resources;
+// namespace App\Filament\Resources;
 
-use App\Filament\Resources\LeaveResource\Pages;
-use App\Filament\Resources\LeaveResource\RelationManagers;
-use App\Models\Leave;
-use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\Resource;
-use Filament\Tables;
-use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
+// use App\Filament\Resources\LeaveResource\Pages;
+// use App\Filament\Resources\LeaveResource\RelationManagers;
+// use App\Models\Leave;
+// use Filament\Forms;
+// use Filament\Forms\Form;
+// use Filament\Resources\Resource;
+// use Filament\Tables;
+// use Filament\Tables\Table;
+// use Illuminate\Database\Eloquent\Builder;
+// use Illuminate\Database\Eloquent\SoftDeletingScope;
 
-class LeaveResource extends Resource
-{
-    protected static ?string $model = Leave::class;
+// class LeaveResource extends Resource
+// {
+//     protected static ?string $model = Leave::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+//     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
-    protected static ?string $navigationGroup = 'System Administration';
+//     protected static ?string $navigationGroup = 'System Administration';
 
-    protected static ?string $navigationLabel = 'Leave Management';
+//     protected static ?string $navigationLabel = 'Leave Management';
 
-    public static function form(Form $form): Form
-    {
-        return $form
-            ->schema([
-                //
-            ]);
-    }
+//     public static function form(Form $form): Form
+//     {
+//         return $form
+//             ->schema([
+//                 //
+//             ]);
+//     }
 
-    public static function table(Table $table): Table
-    {
-        return $table
-            ->columns([
-                //
-            ])
-            ->filters([
-                //
-            ])
-            ->actions([
-                Tables\Actions\EditAction::make(),
-            ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
-            ]);
-    }
+//     public static function table(Table $table): Table
+//     {
+//         return $table
+//             ->columns([
+//                 //
+//             ])
+//             ->filters([
+//                 //
+//             ])
+//             ->actions([
+//                 Tables\Actions\EditAction::make(),
+//             ])
+//             ->bulkActions([
+//                 Tables\Actions\BulkActionGroup::make([
+//                     Tables\Actions\DeleteBulkAction::make(),
+//                 ]),
+//             ]);
+//     }
 
-    public static function getRelations(): array
-    {
-        return [
-            //
-        ];
-    }
+//     public static function getRelations(): array
+//     {
+//         return [
+//             //
+//         ];
+//     }
 
-    public static function getPages(): array
-    {
-        return [
-            'index' => Pages\ListLeaves::route('/'),
-            'create' => Pages\CreateLeave::route('/create'),
-            'edit' => Pages\EditLeave::route('/{record}/edit'),
-        ];
-    }
-}
+//     public static function getPages(): array
+//     {
+//         return [
+//             'index' => Pages\ListLeaves::route('/'),
+//             'create' => Pages\CreateLeave::route('/create'),
+//             'edit' => Pages\EditLeave::route('/{record}/edit'),
+//         ];
+//     }
+// }
